aee6d6dd27a552238b85b22dbfc58eacc2e50735
CHUHAI <haichu7793@gmail.com>
2025-04-23 16:35:58 +0700

    Initialize main folders

---
+++ ShoppiWebParent/ShoppiBackEnd/src/main/java/com/shoppi/admin/ShoppiBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shoppi.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppiBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppiBackEndApplication.class, args);
+	}
+
+}
---
+++ ShoppiWebParent/ShoppiFrontEnd/src/main/java/com/shoppi/site/ShoppiFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shoppi.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppiFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppiFrontEndApplication.class, args);
+	}
+
+}
