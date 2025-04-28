44718fdc9a2784f50c9c6dd6f81f7ed5b674b75c
CHUHAI <haichu7793@gmail.com>
2025-04-28 19:37:11 +0700

    Delete user function

--- ShoppiWebParent/ShoppiBackEnd/src/main/java/com/shoppi/admin/user/UserRepository.java
+++ ShoppiWebParent/ShoppiBackEnd/src/main/java/com/shoppi/admin/user/UserRepository.java
@@ -12,5 +12,7 @@ public interface UserRepository extends CrudRepository<User, Integer>{
 	
 	@Query("SELECT u from User u WHERE u.email = :email")
 	public User getUserByEmail(@Param("email") String email);
+	
+	public Long countById(Integer id);
 
 }
--- ShoppiWebParent/ShoppiBackEnd/src/main/java/com/shoppi/admin/user/UserService.java
+++ ShoppiWebParent/ShoppiBackEnd/src/main/java/com/shoppi/admin/user/UserService.java
@@ -1,8 +1,10 @@
 package com.shoppi.admin.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -31,7 +33,20 @@ public class UserService {
 	}
 	
 	public void save(User user){
-		encodePassword(user);
+		boolean isUpdatingUser = (user.getId() != null);
+		
+		if (isUpdatingUser) {
+			User existingUser = userRepo.findById(user.getId()).get();
+			
+			if (user.getPassword().isEmpty()) {
+				user.setPassword(existingUser.getPassword());
+			} else {
+				encodePassword(user);
+			}
+		} else {
+			encodePassword(user);
+		}
+		
 		userRepo.save(user);
 	}
 	
@@ -41,9 +56,36 @@ public class UserService {
 		user.setPassword(encodePassword);
 	}
 
-	public boolean isEmailUnique(String email) {
+	public boolean isEmailUnique(Integer id, String email) {
 		
 		User userByEmail = userRepo.getUserByEmail(email);	
-		return userByEmail == null;
+		if (userByEmail == null) return true;
+		
+		boolean isCreatingNew = (id==null); // In create new user, id = null
+		
+		if (isCreatingNew) {
+			if (userByEmail != null) return false;
+		} else {
+			if (userByEmail.getId() != id) {
+				return false;
+			}
+		}
+		return true;
 	}
+	
+	public User get(Integer id) throws UserNotFoundException{
+		try {
+			return userRepo.findById(id).get();
+		} catch (NoSuchElementException ex){
+			throw new UserNotFoundException("Could not find any user with ID "+ id);
+			}
+		}
+	
+	public void delete(Integer id) throws UserNotFoundException{
+		Long countById = userRepo.countById(id);
+		if (countById == null || countById==0) {
+			throw new UserNotFoundException("Could not find any user with ID "+ id);
+		}
+		userRepo.deleteById(id);
+		}
 }
