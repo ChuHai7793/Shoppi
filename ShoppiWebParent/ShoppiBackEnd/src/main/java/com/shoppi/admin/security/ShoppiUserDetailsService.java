b01303ce97b5bb4d620365b5a9672e4e0272d85d
CHUHAI <haichu7793@gmail.com>
2025-05-08 20:21:02 +0700

    UserAuthentication,login page, logout link

---
+++ ShoppiWebParent/ShoppiBackEnd/src/main/java/com/shoppi/admin/security/ShoppiUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.shoppi.admin.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.shoppi.admin.user.UserRepository;
+import com.shoppi.common.entity.User;
+
+public class ShoppiUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepo.getUserByEmail(email);
+		if (user != null) {
+			return new ShoppiUserDetails(user);
+		}
+		
+		throw new UsernameNotFoundException("Could not find user with email: " + email);
+	}
+
+}
--- ShoppiWebParent/ShoppiBackEnd/src/main/java/com/shoppi/admin/security/WebSecurityConfig.java
+++ ShoppiWebParent/ShoppiBackEnd/src/main/java/com/shoppi/admin/security/WebSecurityConfig.java
@@ -2,25 +2,72 @@ package com.shoppi.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
 	@Bean
-	public PasswordEncoder PasswordEncoder() {
-		return new BCryptPasswordEncoder();
+	public UserDetailsService userDetailsService() {
+		return new ShoppiUserDetailsService();
 	}
 	
 	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
+	}
+	
+	
+//	@Bean
+//	SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
+//		
+//		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+//		
+//		return http.build();
+//	}
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                   .authenticationProvider(authenticationProvider())
+                   .build();
+    }
+	
+	@Bean
 	SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
-		
-		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-		
+	    http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()
+           )
+           .formLogin(form -> form
+               .loginPage("/login")
+               .usernameParameter("email")
+               .permitAll()
+           );
 		return http.build();
 	}
+	
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+    }
 }
